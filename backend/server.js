46d9e66b4786e2e2016cc35d5b4decf75692dc06
zenlex <erichrkeil@gmail.com>
2022-01-19 22:31:50 -0600

    basic server connection

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,11 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-http.createServer(function (request, response) {
-  console.log('request', request.url)
+const app = http.createServer(function (request, response) {
+  response.writeHead(200, {'Content-Type':'application/json'});
+  response.end(JSON.stringify({message: "Hello World"}))
 })
+
+const PORT = 3001;
+app.listen(PORT);
+
