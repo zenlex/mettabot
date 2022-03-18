d4ef9e8a6077bc6a22fe2f314341f381e46fb759
zenlex <erichrkeil@gmail.com>
2022-03-18 13:05:20 -0500

    public path fix

--- server.js
+++ server.js
@@ -5,38 +5,41 @@ const path = require('path');
 const app = http.createServer(function (request, response) {
   //request logger
   console.log(`request ${request.method} ${request.url}`);
-
-  let filePath = './public' + request.url;
-  if (request.url == '/') {
-    filePath = './public/index.html';
-  }
-
-  const extname = String(path.extname(filePath)).toLowerCase();
-  console.log({extname})
-  var mimeTypes = {
-      '.html' : 'text/html',
-      '.js': 'text/javascript',
-      '.css': 'text/css'
+  if(request.url === '/api/' && request.method === 'GET'){
+    response.writeHead(200, {contentType: 'application/json'})
+    response.end({message: 'Hello World'})
+  } else {
+    let filePath = './public' + request.url;
+    if (request.url == '/') {
+      filePath = './public/index.html';
     }
 
-  const contentType = mimeTypes[extname] || 'application/octet-stream'
-
-  fs.readFile(filePath, function(err, content) {
-    if (err) {
-      if(err.code == 'ENOENT') {
-        fs.readFile('./404.html', function(err, content) {
-          response.writeHead(404, { 'Content-Type': 'text/html '});
-          response.end(content, 'utf-8')
-        })
-      }else {
-        response.writeHead(500)
-        response.end('Sorry, off looking for myself...' + error.code)
+    const extname = String(path.extname(filePath)).toLowerCase();
+    var mimeTypes = {
+        '.html' : 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css'
       }
-    } else {
-      response.writeHead(200, {"Content-Type": contentType});
-      response.end(content, 'utf-8')
-    }
-  });
+    console.log({filePath, extname})
+    const contentType = mimeTypes[extname] || 'application/octet-stream'
+
+    fs.readFile(filePath, function(err, content) {
+      if (err) {
+        if(err.code == 'ENOENT') {
+          fs.readFile('./404.html', function(err, content) {
+            response.writeHead(404, { 'Content-Type': 'text/html '});
+            response.end(content, 'utf-8')
+          })
+        }else {
+          response.writeHead(500)
+          response.end('Sorry, off looking for myself...' + error.code)
+        }
+      } else {
+        response.writeHead(200, {"Content-Type": contentType});
+        response.end(content, 'utf-8')
+      }
+    });
+  }
 })
 
 const PORT = process.env.PORT || 8000;
