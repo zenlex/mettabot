a1befa0d2f1134dea59229b12dd02e61d2e7aa6c
zenlex <erichrkeil@gmail.com>
2022-03-18 11:48:33 -0500

    heroku init deploy

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,14 @@ const fs = require('fs');
 const path = require('path');
 
 const app = http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type':'application/json'});
-  response.end(JSON.stringify({message: "Hello World"}))
+  fs.readFile(__dirname + req.url, function(err, data) {
+    if (err) {
+      response.writeHead(404);
+      response.end(JSON.stringify(err))
+    }
+    response.writeHead(200);
+    response.end(data)
+  });
 })
 
 const PORT = process.env.PORT || 8000;
