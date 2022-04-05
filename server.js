4db94173fc51e0b6e8be094a4d72567d0f74eec7
zenlex <erichrkeil@gmail.com>
2022-04-05 16:17:47 -0500

    add twitter auth request

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const getMsg = require('./messagegen');
+const axios = require('axios');
 
 //MIDDLEWARE
 const app = express();
@@ -25,6 +26,25 @@ app.get('/', (req, res) => {
 app.get('/api', (req, res) => {
   const text = getMsg();
   res.json({ text });
+});
+
+app.get('/api/twitter', (req, res) => {
+  const basicToken = process.env.O2_TWITTER_BASIC_TOKEN;
+  axios.get(
+    'https://twitter.com/i/oauth2/authorize?response_type=code&client_id=VHNyVUF3UXhxRjEtSXpZQndmeVE6MTpjaQ&redirect_uri=https://www.mettabot.app/api/twitter/auth&scope=tweet.read%20users.read%20follows.read%20offline.access&state=state&code_challenge=challenge&code_challenge_method=plain',
+    {
+      headers: {
+        Authorization: `Basic ${basicToken}`,
+      },
+    }
+  );
+});
+
+app.get('/api/twitter/auth', (req, res) => {
+  console.log('response from twitter auth request: ', {
+    body: req.body,
+    params: req.params,
+  });
 });
 
 app.get('/:file', (req, res) => {
